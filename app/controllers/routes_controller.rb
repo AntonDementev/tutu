b049391cc983278cfcb6a19b1fd91f8bc9d26c6a
Anton Dementev <dema309@yandex.ru>
2017-04-04 19:47:37 +0700

    -json

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -23,10 +23,8 @@ class RoutesController < ApplicationController
     respond_to do |format|
       if @route.save
         format.html { redirect_to @route, notice: 'Маршрут был успешно создан.' }
-        format.json { render :show, status: :created, location: @route }
       else
         format.html { render :new }
-        format.json { render json: @route.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -36,10 +34,8 @@ class RoutesController < ApplicationController
     respond_to do |format|
       if @route.update(route_params)
         format.html { redirect_to @route, notice: 'Маршрут был успешно обновлён.' }
-        format.json { render :show, status: :ok, location: @route }
       else
         format.html { render :edit }
-        format.json { render json: @route.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -49,7 +45,6 @@ class RoutesController < ApplicationController
     @route.destroy
     respond_to do |format|
       format.html { redirect_to routes_url, notice: 'Маршрут был успешно удалён.' }
-      format.json { head :no_content }
     end
   end
 
