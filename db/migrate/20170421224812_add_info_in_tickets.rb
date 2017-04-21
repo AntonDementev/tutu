0c84e8041cc55f088507456659b9f323ce020663
Anton Dementev <dema309@yandex.ru>
2017-04-22 06:09:45 +0700

    L 13

---
+++ db/migrate/20170421224812_add_info_in_tickets.rb
@@ -0,0 +1,8 @@
+class AddInfoInTickets < ActiveRecord::Migration[5.0]
+  def change
+    add_belongs_to :tickets, :user
+    add_belongs_to :tickets, :train
+    add_belongs_to :tickets, :first_station
+    add_belongs_to :tickets, :last_station
+  end
+end
