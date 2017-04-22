eeb751a1a583700a549511cd56c6c93d2e41cd3a
Anton Dementev <dema309@yandex.ru>
2017-04-23 01:49:51 +0700

    L13 fix

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,6 +1,7 @@
 class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
-  has_many :trains
-  has_many :tickets
+  has_many :trains, foreign_key: :current_station_id
+  has_many :tickets, foreign_key: :first_station_id
+  has_many :tickets, foreign_key: :last_station_id
 end
