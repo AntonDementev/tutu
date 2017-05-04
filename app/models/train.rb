c01d244f772728815433465030041bfdfdc95f5b
Anton Dementev <dema309@yandex.ru>
2017-05-04 21:15:49 +0700

    перенос вычислений в модель

--- app/models/train.rb
+++ app/models/train.rb
@@ -4,4 +4,28 @@ class Train < ApplicationRecord
   
   has_many :tickets
   has_many :waggons
+  
+  attr_reader :waggons1_amount, :waggon1_places_amount_top, :waggon1_places_amount_bot, :waggons2_amount, :waggon2_places_amount_top, :waggon2_places_amount_bot
+  
+  def update_values
+    @waggons1_amount = 0
+    @waggon1_places_amount_top = 0
+    @waggon1_places_amount_bot = 0
+    @waggons2_amount = 0
+    @waggon2_places_amount_top = 0
+    @waggon2_places_amount_bot = 0
+
+    waggons.each do |waggon|
+      if waggon.category_id == 1
+        @waggons1_amount += 1
+        @waggon1_places_amount_top += waggon.top_places
+        @waggon1_places_amount_bot += waggon.bottom_places
+      elsif waggon.category_id == 2
+        @waggons2_amount += 1
+        @waggon2_places_amount_top += waggon.top_places
+        @waggon2_places_amount_bot += waggon.bottom_places
+      end
+    end
+  end
+  
 end
