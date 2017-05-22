4bcffd40ac8a3feda8cbdf9e81b67763d3621684
Anton Dementev <dema309@yandex.ru>
2017-05-23 00:22:34 +0700

    Delete empty line

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,12 +2,11 @@ class SearchesController < ApplicationController
   before_action :get_stations, only: [:show, :create]
 
   def show
-   
+    
   end
   
   
-  def create
-    
+  def create    
     @begin_station = RailwayStation.find(search_params[:begin_station_id])
     @end_station = RailwayStation.find(search_params[:end_station_id])
     @trains = Search.find_trains(@begin_station, @end_station)
